1c61749f461de97782c6fb76550dca0fed9a3898
YugoOsano <yugo.osano@gmail.com>
2013-10-02 22:15:16 +0900

    pattern match and index added

--- sqlmemo.sql
+++ sqlmemo.sql
@@ -11,10 +11,15 @@ SELECT * FROM
 			       WHERE country = 'Japan'
 			  );
 
-/* show the records that have the highest latitude in each country */
+/* show the records that have the highest latitude in each country 
+   index on country raises the query speed
+*/
 /* correlated subquery is instructed in:
 http://gihyo.jp/dev/serial/01/sql_academy2/000902 
 */
+
+CREATE INDEX IF NOT EXISTS cntidx ON 
+       airports(country); 
  
 SELECT * FROM 
        	airports A1
@@ -24,3 +29,10 @@ SELECT * FROM
 			 WHERE A1.country = A2.country);
 
 
+/* pattern match -- case insensitive */
+/*  
+http://stackoverflow.com/questions/15480319/case-sensitive-and-insensitive-like-in-sqlite
+*/
+
+SELECT * FROM airports
+       	      WHERE country LIKE '%ja%';
