d540f97c106d85eb0a3c85a6c9e532dbedbb50a2
Yugo Osano <yugo.osano@gmail.com>
2014-01-11 10:17:35 +0900

    first commit

---
+++ scrape_from_web.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# scrape html from lecture notes of statistics
+
+# This first scrapes link list in:
+# http://www.snap-tck.com/room04/c01/stat/stat0001.html
+# then scrapes their content and save them into files.
+
+from BeautifulSoup import BeautifulSoup
