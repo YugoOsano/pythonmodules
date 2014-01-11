873c1d532aaef1570636704db8bc1450b2e12f5b
Yugo Osano <yugo.osano@gmail.com>
2014-01-11 10:34:24 +0900

    scrape from lecture note of statistics

--- scrape_from_web.py
+++ scrape_from_web.py
@@ -8,3 +8,36 @@
 # then scrapes their content and save them into files.
 
 from BeautifulSoup import BeautifulSoup
+import urllib
+import re
+
+if __name__ == "__main__":
+
+    urllist = "http://www.snap-tck.com/room04/c01/stat/stat0001.html"
+    f    = urllib.urlopen(urllist)
+    soup = BeautifulSoup(f)
+
+    #-- Use set to skip repeated links --
+    urlset = set()
+
+    #-- extract text within <a> - </a> --
+    for link in soup.findAll('a'):
+        url = link['href']
+
+        #-- extract urls ending with 'stat****.html' (* is digit) --
+        if re.search('stat\d{4}.html$', url):
+            urlset.add('http://www.snap-tck.com/room04/c01/stat' +
+                       url.strip('.'))
+
+    for url in urlset:
+        #-- file name is the end of url --
+        filename = re.search('stat\d{4}.html$', url).group()
+        try:
+            with open(filename, mode='w') as fwrite:
+                print filename, url
+                fread = urllib.urlopen(url)
+                html  = fread.read()
+                fwrite.write(html)
+        except:
+            print url + 'can not open.'
+
