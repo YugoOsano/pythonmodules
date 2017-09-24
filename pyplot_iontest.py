e098c9f8f13d0698ede1a8edeb5254aa67904593
Yugo Osano <yugo.osano@gmail.com>
2017-09-24 14:12:21 +0900

    pyplot updated for Python3.

--- pyplot_iontest.py
+++ pyplot_iontest.py
@@ -6,6 +6,12 @@
 # http://stackoverflow.com/questions/11874767/real-time-plotting-in-while-loop-with-matplotlib
 # http://stackoverflow.com/questions/5541594/matplotlib-draw-showing-nothing
 
+# matplotlib install into Ubuntu
+# sudo apt-get install python-matplotlib
+# sudo apt-get install python3-matplotlib python3-gi-cairo
+
+# https://www.trifields.jp/how-to-install-matplotlib-in-ubuntu-14-04-and-python-3-1173
+
 import matplotlib.pyplot as plt
 
 plt.ion()
@@ -18,5 +24,7 @@ for i in range(10):
     for j in range(10):
         plt.plot([i], [j], 'o')
         plt.draw()
-raw_input("done >>")  
+
+plt.show(block=True) # see StackOverFlow:12358312
+#raw_input("done >>")  
 
