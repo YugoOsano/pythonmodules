f2ad4766cd1831b1af44c98a1284fcbddb65844d
Yugo Osano <yugo.osano@gmail.com>
2019-11-24 13:05:23 +0900

    bayes_inference_beginning now accepts args.

--- bayes_inference_beginning.py
+++ bayes_inference_beginning.py
@@ -1,12 +1,18 @@
+#!/usr/bin/python3
 # coding: utf-8
 # this is based on p-244 of IPython data science cookbook
+# usage: ./bayes_inference_beginning.py [n of trials] [n of coin front]
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from combination import posterior_probability
 
-n = 100
-h = 61
+is_args = (len(sys.argv) == 3)
+
+n = int(sys.argv[1]) if is_args else 100
+h = int(sys.argv[2]) if is_args else 61
+
 q = np.linspace(0.,1., 1000)
 
 fig, ax = plt.subplots(1,1)
