d1260cdfb8d9711afc76894fff421646fd75548f
Yugo Osano <yugo.osano@gmail.com>
2020-01-15 22:13:22 +0900

    practiced getting the last element of a list.

--- generator_test.py
+++ generator_test.py
@@ -24,6 +24,12 @@ print (next(it)) # 4
 #note: Equivalent C++ to Python generator pattern
 #      is answered in StackOverFlow 9059187
 
+# get the last element of a list; see No.4 of the following
+# https://www.geeksforgeeks.org/python-how-to-get-the-last-element-of-list/
+test_list = [1,2,3,4,5]
+rit = reversed(test_list)
+print ("reversed + next applied: ", next(rit))
+
 # https://docs.python.org/ja/3/library/tracemalloc.html
 snapshot = tracemalloc.take_snapshot()
 top_stats = snapshot.statistics('lineno')
