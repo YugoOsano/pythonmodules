cde25c2e3fc2807752a85abeb74efbe3532461e4
Yugo Osano <yugo.osano@gmail.com>
2020-01-18 12:36:18 +0900

    several memo on image handling was added.

--- image_read_show.py
+++ image_read_show.py
@@ -8,6 +8,11 @@ img = plt.imread('http://github.com/ipython-books/cookbook-2nd-data/blob/master/
 
 def show(img):
     fig, (ax1, ax2) = plt.subplots(1,2,figsize=(12,3))
+    # (matplotlib) imshow function plots a 2D raster image.
+    # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.imshow.html
+    # for the term 'raster image' (=bitmap), see
+    # https://en.wikipedia.org/wiki/Raster_graphics
+    # raster is originally scanning beam on television
     ax1.imshow(img, cmap=plt.cm.gray)
     ax1.set_axis_off()
 
