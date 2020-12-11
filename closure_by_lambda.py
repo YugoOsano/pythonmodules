2ab962f837194394c97e91f228681940e3fb3d6f
Yugo Osano <yugo.osano@gmail.com>
2020-12-11 15:28:24 +0900

    multiple statements in lambda was attempted.

--- closure_by_lambda.py
+++ closure_by_lambda.py
@@ -5,3 +5,11 @@ print(f(10))
 a = 20
 print(f(10))
 
+# anonymous use (like C++ [](){}())
+result = (lambda x: x+a)(50)
+print("anonymous ", result)
+
+# multiple statements in a lambda are not permitted in Python
+# https://stackoverflow.com/questions/28429680/lambda-and-multiple-statements-in-python
+fmulti = lambda x: (x+2, x+2)
+print ("fmulti ", fmulti(100)) #-> (102, 102) will be returned
