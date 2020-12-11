bff9d8c080c3a77ab2f5b2460248d01377a375fd
Yugo Osano <yugo.osano@gmail.com>
2020-12-11 15:57:33 +0900

    nested lambda was attempted.

--- closure_by_lambda.py
+++ closure_by_lambda.py
@@ -11,5 +11,9 @@ print("anonymous ", result)
 
 # multiple statements in a lambda are not permitted in Python
 # https://stackoverflow.com/questions/28429680/lambda-and-multiple-statements-in-python
-fmulti = lambda x: (x+2, x+2)
-print ("fmulti ", fmulti(100)) #-> (102, 102) will be returned
+ftuple = lambda x: (x+2, x+2)
+print ("ftuple ", ftuple(100)) #-> (102, 102) will be returned
+
+result = (lambda x: x+2) \
+         ((lambda x: x*5)(50))
+print ("multi ", result)
