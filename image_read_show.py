869d39568a87e9616085fff4198867a61c756d89
Yugo Osano <yugo.osano@gmail.com>
2021-03-25 10:08:44 +0900

    memo on matplotlib histogram.

--- image_read_show.py
+++ image_read_show.py
@@ -19,7 +19,23 @@ def show(img):
     # numpy.ravel() or flatten() function returns one-dimensional array
     # made from all elements of the original multi-dimensional array;
     # a view is returned by ravel() while a copy is returned by flatten().
-    ax2.hist(img.ravel(), lw=0, bins=256)
+
+    # ravel: means disentangle the threads or fibers
+    # type(img): numpy.ndarray
+    # img.ndim -> 2   img.shape -> (288,512) (as tuple)
+    # img.ravel().ndim -> 1  img.ravel().shape -> (147456,)
+
+    # histogram plot: range is a tuple of (x.min, x.max)
+    # matplotlib.pyplot.hist(x, bins=None, range=None, density=False,
+    # weights=None, cumulative=False, bottom=None, histtype='bar',
+    # align='mid', orientation='vertical', rwidth=None, log=False,
+    # color=None, label=None, stacked=False, *, data=None, **kwargs)
+    # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html
+
+    # Python's list can be converted to a numpy array by:
+    # xary = numpy.array(alist, dtype=float)
+    ax2.hist(img.ravel(), bins=256)
+
     ax2.set_xlim(0, img.max())
     ax2.set_yticks([])
     
