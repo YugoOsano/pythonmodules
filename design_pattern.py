e48004f3351ae9ac6b02add026f8810926321eb2
Yugo Osano <yugo.osano@gmail.com>
2021-05-20 17:13:05 +0900

    factory and functor were practiced.

---
+++ design_pattern.py
@@ -0,0 +1,29 @@
+# factory
+class A:
+    def hello(self):
+        print("hello A")
+
+class B:
+    def hello(self):
+        print("hello B")
+
+def factory(is_a: bool):
+    if (is_a): return A()
+    return B()
+
+for is_a in [True, False]:
+    c = factory(is_a)
+    print(type(c))
+    c.hello()
+
+# functor
+class F:
+    def __init__(self, name: str):
+        self.name = name
+    def __call__(self, n: int):
+        for i in range(n):
+            print(self.name)
+
+#f = F() # error
+f = F('hoge')
+f(5)
