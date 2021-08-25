562a0ddcc781b6c82257c5d38a6000bf5c9aa4a9
Yugo Osano <yugo.osano@gmail.com>
2021-08-25 16:58:45 +0900

    shell practiced on for loop.

--- shell_basic.sh
+++ shell_basic.sh
@@ -11,8 +11,12 @@ for i in {1..5}
 do
     echo "hello $i times"
     a=$a$i #character concatenation
+    # file creation
+    echo "hello" > tmpfile$i.txt
 done
 echo "string $a"
+ls tmpfile*
+rm tmpfile*
 
 # making a string from ascii code
 # https://orebibou.com/2017/03/bash%E3%81%A7%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89%E3%82%92%E3%82%A2%E3%82%B9%E3%82%AD%E3%83%BC%E3%82%B3%E3%83%BC%E3%83%89%E3%81%A7%E8%A8%98%E8%BF%B0%E3%81%97%E3%81%A6%E5%AE%9F%E8%A1%8C%E3%81%95/
