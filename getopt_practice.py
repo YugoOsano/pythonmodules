ca30069c17ef3723ab0e6407d5dad71e3a5fb1a9
Yugo Osano <yugo.osano@gmail.com>
2022-05-13 14:59:47 +0900

    comment added to getopt_practice.

--- getopt_practice.py
+++ getopt_practice.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # ./getopt_practice.py Hello World -> ['Hello', 'World'] as args
 # ./getopt_practice.py -f Hello -l World
@@ -13,6 +13,9 @@ def full_name():
     last_name  = ""
     argv = sys.argv[1:]
 
+    # here, "f:l:" is shortopts; the string of option letters that the script wants to recognize,
+    # with options that require an argument followed by a colon
+    # (':'; i.e., the same format that Unix getopt() uses).
     try:
         opts,args = getopt.getopt(argv, "f:l:")
     except:
