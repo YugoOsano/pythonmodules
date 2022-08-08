17cf1684fbbc672e867892f836d47f19ac6720ab
Yugo Osano <yugo.osano@gmail.com>
2022-08-08 13:35:45 +0900

    base64 step added to AES decryption.

--- aes_crypto/decrypt.py
+++ aes_crypto/decrypt.py
@@ -1,7 +1,14 @@
+# usage
+# cat [encrypted text] | python3 decrypt.py
+
 import sys
 import getpass
 import my_aes
+import base64
 
 password = getpass.getpass('password> ')
-dec = my_aes.decrypt(sys.stdin.buffer.read(), password)
+
+txt     = sys.stdin.buffer.read()
+to_read = base64.b64decode(txt)
+dec = my_aes.decrypt(to_read, password)
 sys.stdout.buffer.write(dec)
