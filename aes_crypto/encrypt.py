50e5ca963bf46c2062a994696bb44cba62b7466f
Yugo Osano <yugo.osano@gmail.com>
2022-08-22 15:37:23 +0900

    AES encryption practiced by openssl

--- aes_crypto/encrypt.py
+++ aes_crypto/encrypt.py
@@ -14,3 +14,11 @@ if password != password2:
 enc = my_aes.encrypt(sys.stdin.buffer.read(), password)
 to_write = base64.b64encode(enc)
 sys.stdout.buffer.write(to_write)
+
+# similar encryption gained by:
+# cat [file to encrypt] | openssl aes-256-cbc -base64 -pbkdf2 > [file of encrypted text]
+
+# decrypted by:
+# cat [file of encrypted text] | openssl aes-256-cbc -d -base64 -pbkdf2 > [file of original text]
+
+# options are shown by  openssl help
