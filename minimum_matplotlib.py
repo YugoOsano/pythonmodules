2d73d2177b93fbf12ff63ec13e3031424845c08a
Yugo Osano <yugo.osano@gmail.com>
2023-02-22 15:39:24 +0900

    plot connected by lines in matplotlib

--- minimum_matplotlib.py
+++ minimum_matplotlib.py
@@ -3,6 +3,8 @@ plt.grid(visible=True)
 plt.plot([1,2,4])
 #plt.scatter(xlist,ylist)  for scatter plot such as
 # plt.scatter([1,2],[3,4])
+# https://www.delftstack.com/ja/howto/matplotlib/how-to-connect-scatterplot-points-with-line-in-matplotlib/
+# plt.plot([1,2],[3,4]) to connect by lines, both scatter and plot for both
 
 # value ranges are set right before show()
 #plt.xlim([-1,6])
